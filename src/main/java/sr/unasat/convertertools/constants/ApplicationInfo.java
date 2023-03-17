3489e13a8b272ea251ff4172647a19339a7c9931
Girish Oemrawsingh <girish-io@protonmail.com>
2023-03-17 20:09:04 -0300

    Bumped version to 1.0.4

--- src/main/java/sr/unasat/convertertools/constants/ApplicationInfo.java
+++ src/main/java/sr/unasat/convertertools/constants/ApplicationInfo.java
@@ -1,6 +1,6 @@
 package sr.unasat.convertertools.constants;
 
 final public class ApplicationInfo {
-    public final static String VERSION = "v1.0.3";
+    public final static String VERSION = "v1.0.4";
     public final static String WINDOW_TITLE = "Converter Tools";
 }
